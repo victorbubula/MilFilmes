a77baaf2c59581ec0272c1a5d7a26a305e8f7c25
victorbubula <162257803+victorbubula@users.noreply.github.com>
2024-03-05 18:40:47 -0300

    update funcional

--- src/pages/add/index.tsx
+++ src/pages/add/index.tsx
@@ -9,45 +9,10 @@ export default function Add() {
 	const [title, setTitle] = useState('');
 	const [img, setImg] = useState('');
 	
-	/*async function lerArquivoJSON() {
-		const resposta = await fetch('https://api.npoint.io/83dd26caf95c5aa77f11');
-		const dados = await resposta.json();
-		return dados;
-	}
-		
-	// Função para modificar o arquivo JSON
-	async function modificarArquivoJSON(novoTitulo:string) {
-		// Lê o arquivo JSON
-		const dados = await lerArquivoJSON();
-		
-		// Modifica o valor da idade
-		dados.title = novoTitulo;
-		
-		// Salva as alterações de volta no arquivo JSON
-		const resposta = await fetch('https://api.npoint.io/83dd26caf95c5aa77f11', {
-			method: 'PUSH', // Pode variar dependendo do servidor
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(dados)
-		});
-		
-		// Se desejar, você pode verificar a resposta do servidor
-		console.log(resposta);
-	}*/
-		
-	const onSave = (event:any) => {
-		event.preventDefault();
-		//modificarArquivoJSON(title);
-		alert('filme adicionado com sucesso');
-		setTitle('');
-		setImg('');
-	};
-
 	return <div className={styles.background}>
 		<header className={styles.header}><Logo/></header>
 		<main>
-			<form onSubmit={onSave}>
+			<form>
 				<Input
 					label="Titulo"
 					placeholder="Insira o titulo do filme"
@@ -60,7 +25,6 @@ export default function Add() {
 					onChange={value => setImg(value)}
 					value={img}
 				/>
-				
 				<Button type='submit'>Adicionar</Button>
 			</form>
 		</main>
