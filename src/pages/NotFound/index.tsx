e2ef70c3400cb33259b5dbfa59ce5c20294e472b
victorbubula <162257803+victorbubula@users.noreply.github.com>
2024-03-10 03:00:05 -0300

    navigate
    btn voltar

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,9 +1,14 @@
 import styles from './NotFound.module.scss';
 import { ReactComponent as NotFoundImage } from 'assets/not_found.svg';
+import { useNavigate } from 'react-router-dom';
 
 export default function NotFound() {
+	const navigate = useNavigate();
 	return (
 		<div className={styles.container}>
+			<button onClick={() => navigate(-1)}>
+				{'< voltar'}
+			</button>
 			<NotFoundImage/>
 			<h1>Página não encontrada</h1>
 		</div>
