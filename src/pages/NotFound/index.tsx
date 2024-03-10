4b9c20f62f246537893a5c06e83e5521c844aaae
victorbubula <162257803+victorbubula@users.noreply.github.com>
2024-03-10 01:07:05 -0300

    okok

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -4,6 +4,7 @@ export default function NotFound() {
 	return (
 		<div className={styles.container}>
 			<img src="assets/not_found.png" alt="pagina nao encontrada" />
+			<h1>Página não encontrada</h1>
 		</div>
 	);
 }
