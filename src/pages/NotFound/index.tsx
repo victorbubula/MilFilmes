71784f56d9863f9da4e8b42e880f7dcad0b9e0df
victorbubula <162257803+victorbubula@users.noreply.github.com>
2024-03-10 01:21:21 -0300

    ok

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,9 +1,10 @@
 import styles from './NotFound.module.scss';
+import { ReactComponent as NotFoundImage } from 'assets/not_found.svg';
 
 export default function NotFound() {
 	return (
 		<div className={styles.container}>
-			<img src="assets/not_found.png" alt="pagina nao encontrada" />
+			<NotFoundImage/>
 			<h1>Página não encontrada</h1>
 		</div>
 	);
