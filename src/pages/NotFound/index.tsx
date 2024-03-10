0e977a0956c717193c3234e313bb84bebb88d6b9
victorbubula <162257803+victorbubula@users.noreply.github.com>
2024-03-10 01:01:02 -0300

    okok

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,9 +1,9 @@
 import styles from './NotFound.module.scss';
 
 export default function NotFound() {
-    return(
-        <div className={styles.container}>
-            <img src="assets/not_found.png" alt="pagina nao encontrada" />
-        </div>
-    );
+	return (
+		<div className={styles.container}>
+			<img src="assets/not_found.png" alt="pagina nao encontrada" />
+		</div>
+	);
 }
